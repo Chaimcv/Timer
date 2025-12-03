01559859d74a1594f53bda927af18f6f852a2949
Chaimcv <chaithanyamcv@gmail.com>
2025-12-03 20:32:59 +0530

    done

---
+++ timer.js
@@ -0,0 +1,23 @@
+  let counter=0;
+        let out=null;
+        var display=document.getElementById("display");
+        const playFn=()=>{
+           // if(out)
+             //   return;
+          out= setInterval(()=>{
+                 display.innerHTML=counter;
+            counter++
+             },1000);    
+    }
+    
+    function stop(){
+         clearInterval(out);
+         out=null;
+         counter=0;
+         display.innerHTML=counter;
+    }
+    function pause(){
+        clearInterval(out);
+        out=display.innerHTML;
+
+    }
